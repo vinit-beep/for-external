085fe2d25f21e1b0a523de42cfe600d5e11de31f
vinit-beep <vinitkukiadiya1@gmail.com>
2023-02-10 11:36:54 +0530

    commit php file

---
+++ index2.php
@@ -0,0 +1,8 @@
+<footer style="margin-bottom: 0px;"> 
+           <div class="footer-lg bg-footer text-center">
+                   <div class="container"> 
+                            <p class="m-0 font-12">Copyright © 2022 Radixweb. All Rights Reserved</p> 
+                   </div> 
+            </div> 
+</footer>
+
